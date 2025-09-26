25e831316c8cad4eaa6200d11fa7f971c27f32bc
cesar <cesar@MacBook-Pro-de-Cesar.local>
2025-09-26 14:25:18 -0600

    feat: :lipstick: Añadición del archivo Python del parpadeo de led

---
+++ led_high.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(17, GPIO.OUT)
+
+def loop():
+    GPIO.output(17, GPIO.HIGH)
+    time.sleep(0.5)  
+    GPIO.output(17, GPIO.LOW)
+    time.sleep(0.5)
+
+if __name__ == "__main__":
+    try:
+        setup()
+        while True:
+            loop()
+    except KeyboardInterrupt:
+        print("Programa terminado")
+        GPIO.cleanup()
