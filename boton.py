f5683f09e2824eefed970d5f4d73bbc025609b73
cesar <cesar@MacBook-Pro-de-Cesar.local>
2025-09-26 14:43:41 -0600

    feat: :lipstick: Se añadió el archivo boton.py

---
+++ boton.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+
+LED_PIN = 4 
+BTN_PIN = 14
+
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(LED_PIN, GPIO.OUT)
+    GPIO.setup(BTN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def loop():
+    if GPIO.input(BTN_PIN) == GPIO.LOW:
+        GPIO.output(LED_PIN, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(LED_PIN, GPIO.LOW)
+        time.sleep(0.5)
+    else:                     
+        GPIO.output(LED_PIN, GPIO.LOW)
+
+if __name__ == "__main__":
+    try:
+        setup()
+        while True:
+            loop()
+    except KeyboardInterrupt:
+        print("Programa terminado")
+    finally:
+        GPIO.cleanup()
